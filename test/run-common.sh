f8241463e5e0c9d6269d84b1189c10a26764c22b
Abhishek N Sharma <abhishek.n.sharma@ni.com>
2022-08-15 13:36:25 +0530

    added roundtrip test

---
+++ test/run-common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set network_enabled = true in ecal.ini.
+# You can omit this, if you only need local communication.
+PREFIX=/usr/local
+NAME=$1
+if [ -z "${NAME}" ]; then
+   NAME=sender
+fi
+MODE="local"
+if [ "x${MODE}" == "xlocal" ]; then
+
+else
+        awk -F"=" '/^network_enabled/{$2="= true"}1' ${PREFIX}/etc/ecal/ecal.ini > ${PREFIX}/etc/ecal/ecal.tmp && \
+                rm ${PREFIX}/etc/ecal/ecal.ini && \
+                mv ${PREFIX}/etc/ecal/ecal.tmp ${PREFIX}/etc/ecal/ecal.ini
+        rm -f /logs/${NAME}.log
+fi
+
+${PREFIX}/bin/ecal_sample_${NAME} | tee /logs/${NAME}.log
+#sleep 60
