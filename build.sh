e6c91bd86eab40bf6b1179a7519aa615b3c96ea9
Abhishek N Sharma <abhishek.n.sharma@ni.com>
2022-08-17 16:52:17 +0530

    segregated local and network tests.. set build=release

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ git config --global --add safe.directory /ecal
 
 mkdir -p build
 #cd build
-#iceoryx_posh_DIR=/ecal/thirdparty/iceoryx/build/install/prefix iceoryx_hoofs_DIR=/ecal/thirdparty/iceoryx/build/install/prefix iceoryx_dust_DIR=/ecal/thirdparty/iceoryx/build/install/prefix cmake .. -DHAS_QT5=OFF  
+#iceoryx_posh_DIR=/ecal/thirdparty/iceoryx/build/install/prefix iceoryx_hoofs_DIR=/ecal/thirdparty/iceoryx/build/install/prefix iceoryx_dust_DIR=/ecal/thirdparty/iceoryx/build/install/prefix cmake .. -DHAS_QT5=OFF
 #make -j 8 install
 
 cd build
 #cmake .. -DCMAKE_BUILD_TYPE=Debug -DECAL_THIRDPARTY_BUILD_PROTOBUF=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -DECAL_THIRDPARTY_BUILD_QWT=OFF -DHAS_QT5=OFF -DHAS_ICEORYX=OFF
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DECAL_THIRDPARTY_BUILD_QWT=OFF -DHAS_QT5=OFF -DHAS_ICEORYX=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Release -DECAL_THIRDPARTY_BUILD_QWT=OFF -DHAS_QT5=OFF -DHAS_ICEORYX=OFF
 make -j8
 
 cpack -G DEB
---
+++ localtests/run-common.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set network_enabled = true in ecal.ini.
+# You can omit this, if you only need local communication.
+PREFIX=/usr/local
+NAME=$1
+if [ -z "${NAME}" ]; then
+   NAME=sender
+fi
+
+# set to local if running all containers in one pc.
+MODE="local"
+
+if [ "x${MODE}" == "xlocal" ]; then
+        echo "no n/w settings..."
+else
+        awk -F"=" '/^network_enabled/{$2="= true"}1' ${PREFIX}/etc/ecal/ecal.ini > ${PREFIX}/etc/ecal/ecal.tmp && \
+                rm ${PREFIX}/etc/ecal/ecal.ini && \
+                mv ${PREFIX}/etc/ecal/ecal.tmp ${PREFIX}/etc/ecal/ecal.ini
+        rm -f /logs/${NAME}.log
+fi
+
+${PREFIX}/bin/ecal_sample_${NAME} | tee /logs/${NAME}.log
